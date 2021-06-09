42c7243ae66f760835770612c38055719929f4c2
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-06-09 22:23:59 +0900

    ユーザー管理機能1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :email, :first_name_full_width, :family_name_full_width, :first_name_kana, :family_name_kana, :birthday]
+    )
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :purchase
+
+  with_options presence: true do
+      validates :nickname
+
+  with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :family_name_full_width
+      validates :first_name_full_width
+    end
+
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+
+      validates :birthday
+   end
+end
