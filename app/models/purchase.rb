c168f5ed35ac9bd7148730c293e019d7598f770a
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-06-18 11:35:39 +0900

    商品一覧機能訂正4

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,12 +1,12 @@
-class Purchase < ApplicationRecord
-  belongs_to :item
-  belongs_to :user
-  has_one :purchase
-  has_one_attached :image
+# class Purchase < ApplicationRecord
+#   belongs_to :item
+#   belongs_to :user
+#   has_one :purchase
+#   has_one_attached :image
 
-  validates :content, presence: true, unless: :was_attached?
+#   validates :content, presence: true, unless: :was_attached?
 
-  def was_attached?
-    self.image.attached?
-  end
-end
+#   def was_attached?
+#     self.image.attached?
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
  
   root to: "items#index"
     resources :items do
-    resources :item, only:[:new]
-    resources :orders, only: [:create, :index]
+    
+
     resources :comments, only: :create
   end
 
