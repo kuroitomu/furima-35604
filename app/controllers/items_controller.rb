a75fffdd0c567c888640d20a27db8c0c64b7265f
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-06-21 20:59:10 +0900

    商品情報編集機能1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,18 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
-  # before_action :prohibit_access, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :prohibit_access, only: [:edit, :update, :destroy]
 
   def index
       @items = Item.order("created_at DESC")
   end
 
   def new
-    @item = Item.new
+     @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params) 
+     @item = Item.new(item_params) 
     if @item.save
        redirect_to root_path
     else
@@ -21,21 +21,20 @@ class ItemsController < ApplicationController
   end
 
   def show
-
+     
   end
 
-  # def edit 
+   def edit 
+  
+  end
 
-  # end
-
-  # #def update
-  #   if @item.update(item_params)
-  #      @item.valid?
-  #      redirect_to action: :show
-  #   else
-  #      render :edit
-  #   end
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+       render :edit
+    end
+  end
 
   # def destroy
   #   if @item.destroy
@@ -55,9 +54,9 @@ class ItemsController < ApplicationController
      @item = Item.find(params[:id])
    end
 
-  # def prohibit_access
-  #   redirect_to action: :show if @item.user_id != current_user.id
-  # end
+  def prohibit_access
+    # redirect_to action: :show if @item.user_id != current_user.id
+  end
   
   def move_to_index
     # redirect_to action: :index if (@item.user_id != current_user.id|| @item.purchase.present?)
