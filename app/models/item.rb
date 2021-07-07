5a0e47782415f203749c1eb6839c2f8994f2352b
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-07-07 12:07:52 +0900

    最終課題訂正4

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,20 +11,20 @@ class Item < ApplicationRecord
    has_one          :purchase
    has_one_attached :image
    has_one          :order
+   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
-with_options presence: true do
-  validates :image
-  validates :name
-  validates :product_explanation
-  validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+ with_options presence: true do
+   validates :image
+   validates :name
+   validates :product_explanation
+   validates :price, format: { with: /\A[0-9]+\z/ }
 
-with_options numericality: { other_than: 1 } do
-  validates :category_id
-  validates :product_status_id
-  validates :shipping_charges_id
-  validates :prefecture_id
-  validates :shipping_time_id
+ with_options numericality:  { other_than: 1 } do
+   validates :category_id
+   validates :product_status_id
+   validates :shipping_charges_id
+   validates :prefecture_id
+   validates :shipping_time_id
     end
   end 
 end
