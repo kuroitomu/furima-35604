b27a1a59fe38c40ee52450b1a451c17896cd2343
kuroitomu <gin0uu.u1d.basuzur10@gmail.com>
2021-07-11 16:40:49 +0900

    Capfileを編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,13 @@ lock '3.16.0'
 set :application, 'furima-35604'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:Githubのkuroitomu/furima-35604.git'
+set :repo_url,  'git@github.com:kuroitomu/furima-35604.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '’2.6.5’' #カリキュラム通りに進めた場合、’2.6.5’ です
+set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
